9ec268ce7304bc70e2fd9afdf8868d23f9d6aa6d
Bertrand Putigny <bertrand.putigny@gmail.com>
2015-02-09 11:16:52 +0100

    Version reports HDF5 and PGPLOT support

--- src/main/version.cpp
+++ src/main/version.cpp
@@ -2,6 +2,18 @@
 #include "stdio.h"
 
 int main() {
-	printf("ESTER %s\n", PACKAGE_VERSION);
+	printf("ESTER version %s\n", PACKAGE_VERSION);
+#ifdef USE_HDF5
+    const char *h5supp = "yes";
+#else
+    const char *h5supp = "no";
+#endif
+#ifdef USE_PGPLOT
+    const char *pgsupp = "yes";
+#else
+    const char *pgsupp = "no";
+#endif
+	printf("HDF5 support: %s\n", h5supp);
+	printf("PGPLOT support: %s\n", pgsupp);
     return 0;
 }
