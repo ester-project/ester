7f9f709b6468ad4a469e805225def3765cf1e31d
Michel Rieutord <rieutord@spica>
2017-04-26 13:13:53 +0200

    star1d_solvers.cpp still at work

--- src/include/constants.h
+++ src/include/constants.h
@@ -6,6 +6,7 @@
 extern const double PI;
 extern const double SIG_SB;
 extern const double K_BOL;
+extern const double UMA;
 extern const double HYDROGEN_MASS;
 extern const double A_RAD;
 extern const double GRAV;
