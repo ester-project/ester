ester-project/ester

commit bde4ddecfebfc3a70862f2b2f2b720f5665a9702

Adds solve_phi function
solve_phi solves Poisson equation for the gravitational potential. It is
used to initialize phi based on density profile when reading an input
model.

src/star/star2d_class.cpp
@@ -9,6 +9,67 @@
 #endif
 #include "matplotlib.h"
 
+matrix star2d::solve_phi() {
+    symbolic S;
+    S.set_map(map);
+
+    sym symPhi = S.regvar("Phi");
+    matrix phi, rho;
+
+    phi = zeros(nr, nth);
+    rho = this->rho;
+
+    sym eq = lap(symPhi);
+    solver op;
+    op.init(ndomains, 1, "full");
+    op.regvar("Phi");
+    op.set_nr(map.npts);
+
+    S.set_value("Phi", phi);
+
+    op.reset();
+    op.set_nr(map.npts);
+
+    matrix rhs = rho*pi_c;
+
+    eq.add(&op, "Phi", "Phi");
+
+    int j0 = 0;
+    for (int n=0; n<ndomains; n++) {
+        if (n == 0) {
+            // BC at center
+            op.bc_bot2_add_l(0, "Phi", "Phi", ones(1, nth), map.D.block(0).row(0));
+            rhs(0) = 0.0;
+        }
+        else {
+            op.bc_bot2_add_l(n, "Phi", "Phi", ones(1,1), D.block(n).row(0));
+            op.bc_bot1_add_l(n, "Phi", "Phi", -ones(1,1), D.block(n-1).row(-1));
+            rhs(j0) = -(D, phi)(j0) + (D, phi)(j0-1);
+        }
+
+        if (n == ndomains-1) {
+            // BC at surface
+            op.bc_top1_add_l(n, "Phi", "Phi", ones(1, nth), map.D.block(n).row(-1));
+            op.bc_top1_add_d(n, "Phi", "Phi", ones(1, nth));
+            rhs(-1) = -(map.D, phi)(-1) - phi(-1);
+        }
+        else {
+            op.bc_top1_add_d(n,"Phi", "Phi", ones(1,1));
+            op.bc_top2_add_d(n,"Phi", "Phi", -ones(1,1));
+            rhs(j0+map.gl.npts[n]-1) = -phi(j0+map.gl.npts[n]-1)+phi(j0+map.gl.npts[n]);
+        }
+        j0 += map.gl.npts[n];
+    }
+
+    op.set_rhs("Phi", rhs);
+
+    op.solve();
+
+    phi = op.get_var("Phi");
+
+    return phi;
+}
+
 star2d::star2d() : nr(map.gl.N), nth(map.leg.npts), nex(map.ex.gl.N),
     ndomains(map.gl.ndomains), r(map.r), z(map.z), th(map.th), Dt(map.Dt),
     Dt2(map.Dt2), zex(map.ex.z), Dex(map.ex.D), rex(map.ex.r),
@@ -469,10 +530,12 @@ int star2d::hdf5_read(const char *input_file, int dim) {
 
     fill();
 
+    matrix sol_phi = solve_phi();
+    phi = sol_phi;
+
     return 0;
 #else
     ester_err("Could not read from hdf5 file: HDF5 is not enabled");
-    return 1; // return 1 (failure) if HDF5 is not enabled
 #endif
 }
 
@@ -921,14 +984,16 @@ void star2d::init1d(const star1d &A,int npts_th,int npts_ex) {
 }
 
 void star2d::interp(remapper *red) {
+
     p=red->interp(p);
+
     phi=red->interp(phi);
     T=red->interp(T);
     w=red->interp(w);
     G=red->interp(G,11);
     comp=red->interp(comp);
     phiex=red->interp_ex(phiex);
-    fill(); // recompute the microphysic variables
+    fill(); // recompute the micro-physics variables
 
 }
 

src/include/star.h
@@ -164,6 +164,8 @@ class star2d {
     void hdf5_write(const char *filename) const;
     int hdf5_read(const char *input_file, int dim);
 
+    matrix solve_phi(); // used to calculate initial solution for phi based on rho
+
     virtual void plot(const matrix_map&);
 };
 

src/include/utils.h
@@ -58,4 +58,13 @@
 
 bool isHDF5Name(const char *fileName);
 
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define RESET "\033[0;0m"
+
+#define LOGI(...)   { printf(GREEN); printf(__VA_ARGS__); printf(RESET); fflush(stdout); }
+#define LOGW(...)   { fprintf(stderr, YELLOW); fprintf(stderr, __VA_ARGS__); fprintf(stderr, RESET); }
+#define LOGE(...)   { fprintf(stderr, RED); fprintf(stderr, __VA_ARGS__); fprintf(stderr, RESET); }
+
 #endif // ESTER_UTILS_H

src/main/star1d.cpp
@@ -40,8 +40,6 @@ int main(int argc,char *argv[]) {
 
     signal(SIGINT, sig_handler);
 
-    plt::figure(1, 10, 4);
-
     t.start();
     // if(config.verbose) {
     //     fig=new figure(config.plot_device);
@@ -85,9 +83,13 @@ int main(int argc,char *argv[]) {
 
     int last_plot_it = -100;
 
-    if (config.noplot == false) A.plot(error_map.block(0, nit-1, 0 ,0));
+    if (config.noplot == false) {
+        plt::figure(1, 10, 4);
+        A.plot(error_map.block(0, nit-1, 0 ,0));
+    }
 
     while(!last_it) {
+
         if(err<0.1&&!*config.input_file) {
             A.core_convec=core_convec_set;
             A.env_convec=env_convec_set;
@@ -102,18 +104,9 @@ int main(int argc,char *argv[]) {
         last_it=(err<config.tol&&nit>=config.minit)||nit>=config.maxit || killed;
         if(config.verbose) {
             printf("it=%d err=%e\n",nit,err);
-
-            // if(tt(nit-1)-t_plot>config.plot_interval||last_it) {
-            //     fig->semilogy(error.block(0,nit-1,0,0));
-            //     fig->label("Iteration number","Relative error","");
-            //     A.spectrum(fig,A.rho);
-            //     fig->label("Density (normalized spectrum)","","");
-            //     t_plot=tt(nit-1);
-            // }
-
         }
 
-        if (config.noplot == false && (nit - last_plot_it > 10 || last_it)) {
+        if (config.noplot == false && (nit+1 - last_plot_it > config.plot_interval || last_it)) {
             last_plot_it = nit;
             A.plot(error_map.block(0, nit-1, 0 ,0));
         }

src/star/star1d_class.cpp
@@ -265,7 +265,9 @@ int star1d::init(const char *input_file,const char *param_file,int argc,char *ar
 	
 	init_comp();
 	fill();
-	
+
+    phi = solve_phi();
+
 	return 0;
 }
 

src/star/star1d_solvers.cpp
@@ -11,6 +11,7 @@ void star1d::fill() {
     init_comp();
 
     eq_state();
+
     opacity();
     nuclear();
 
@@ -166,6 +167,7 @@ double star1d::solve(solver *op, matrix_map& error_map, int nit) {
     phi+=h*dphi;
     p+=h*dp*p;
     T+=h*dT*T;
+
     pc*=exp(h*dpc(0));
     Tc*=exp(h*dTc(0));
 

src/star/star_map.cpp
@@ -213,6 +213,15 @@ matrix star2d::find_boundaries(const matrix &logTi) const {
             zj+=dzj;
             nit++;
             if(nit>100) {
+                plt::clf();
+                plt::plot(r, log(T), "$T$");
+                for (int i=0; i<logTi.nrows(); i++) {
+                    plt::axvline(zj(i));
+                    plt::axhline(logTi(i));
+                    LOGE("ri%d zj=%e, dzj=%e\n", i, zj(i), dzj(i));
+                }
+                LOGE("No convergence in find_boundaries");
+                plt::show(true);
                 ester_err("No convergence in find_boundaries\n");
             }
         }

commit 1d5e36893cd3e15313dc2200b92efbe8c85ef678

Fix bug introduced in bde4ddecfebfc3a70862f2b2f2b720f5665a9702

src/star/star2d_class.cpp
@@ -515,9 +515,6 @@ int star2d::hdf5_read(const char *input_file, int dim) {
 
     fill();
 
-    matrix sol_phi = solve_phi();
-    phi = sol_phi;
-
     return 0;
 #else
     ester_err("Could not read from hdf5 file: HDF5 is not enabled");