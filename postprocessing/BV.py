b5ebd5b4efe5a1e5df90baa5d9cff86ac58e887c
Michel Rieutord <mrieutord@irap.omp.eu>
2019-04-04 08:25:20 +0200

    Little corrections in postprocessing/BV.py

--- postprocessing/BV.py
+++ postprocessing/BV.py
@@ -5,14 +5,14 @@ from ester import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-a=star2d('Achernar2d')
+a=star2d('/home/rieutord/Ester/local/runs/Master/M3O7.h5')
 
 Req=a.Re
 Rpol=a.Rp
 print Req/R_SUN,Rpol/R_SUN
 
-x=a.r*np.sin(a.th)/Req
-y=a.r*np.cos(a.th)/Req
+x=a.r*np.sin(a.th)
+y=a.r*np.cos(a.th)
 x=np.c_[x,-x[:,-2::-1],-x[:,1:],x[:,-2::-1]]
 y=np.c_[y,y[:,-2::-1],-y[:,1:],-y[:,-2::-1]]
 z=np.log10(np.c_[a.N2,a.N2[:,-2::-1],a.N2[:,1:],a.N2[:,-2::-1]])
