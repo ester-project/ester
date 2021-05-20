fa46b8310fdaac5cd3244df8af03fa931130c626
3434 <mombarg@olympelogin1.bullx>
2021-05-20 16:03:27 +0200

    OP mono added in physics

---
+++ src/physics/opa_opmono.cpp
@@ -0,0 +1,75 @@
+#ifndef WITH_CMAKE
+#include "ester-config.h"
+#endif
+#include <cmath>
+#include <string.h>
+#include "matrix.h"
+#include "constants.h"
+#include "physics.h"
+
+#include <iostream>
+using namespace std;
+
+extern"C" {
+    //int init_opmono_opa_();
+    void opa_opmono_(double *, double *,
+            double *, double *, double *, double *, double *);
+}
+
+int opa_opmono(const matrix& X, double Z, const matrix& T, const matrix& rho,
+		opa_struct& opa) {
+
+    int error = 0;
+    static bool init = false;
+
+	opa.k.dim(T.nrows(), T.ncols());
+    opa.xi.dim(T.nrows(), T.ncols());
+    opa.dlnxi_lnrho.dim(T.nrows(), T.ncols());
+    opa.dlnxi_lnT.dim(T.nrows(), T.ncols());
+
+//    if (!init) {
+//        init_opmono_opa_();
+//    }
+
+    for (int i=0; i<X.nrows(); i++) {
+        for (int j=0; j<X.ncols(); j++) {
+            double x[3];
+            double t, ro, kap, dkapt, dkapro, dkapx;
+            double dlnkT;
+
+            x[0] = X(i, j);
+            x[1] = 1.0 - X(i, j) - Z;
+            x[2] = Z;
+            // x[3] = 3.425E-03;
+            // x[4] = 4.128E-05;
+            // x[5] = 1.059E-03;
+            // x[6] = 4.167E-06;
+            // x[7] = 9.640E-03;
+            // x[8] = 3.903E-06;
+            // x[9] = 5.827E-03;
+
+            t = T(i, j);
+            ro = rho(i, j);
+
+            opa_opmono_(x, &t, &ro, &kap, &dkapt, &dkapro, &dkapx);
+
+            // opa.k(i, j) = kap;
+            // opa.xi(i, j) = 16*SIG_SB*pow(t,3)/(3*kap*ro);
+            // opa.dlnxi_lnrho(i, j) = -1.0 - ro/kap * dkapro;
+            // opa.dlnxi_lnT(i, j) = 3.0 - t/kap * dkapt;
+
+            opa.k(i, j)=pow(10,kap);
+ 	        dlnkT-=3*dkapro;
+	        opa.xi(i, j)=16*SIG_SB*pow(t,3)/(3*opa.k(i, j)*ro);
+	        opa.dlnxi_lnrho(i ,j)=(-1-dkapro);
+            opa.dlnxi_lnT(i, j)=(3-dkapt);  
+
+
+        }
+    }
+    printf("Centre:  T = %e rho = %e kap = %e, dlnxi_lnT = %e, dlnxi_lnrho = %e \n", T(1), rho(1), opa.k(1), opa.dlnxi_lnT(1), opa.dlnxi_lnrho(1));          
+    printf("Surface: T = %e rho = %e kap = %e, dlnxi_lnT = %e, dlnxi_lnrho = %e \n", T(-1), rho(-1), opa.k(-1), opa.dlnxi_lnT(-1), opa.dlnxi_lnrho(-1));          
+
+	return error;
+
+}
