corrected missing factor rho in Dv

postprocessing/spectrum2d.py
@@ -1,14 +1,16 @@
 # Compute the 2D Chebyshev Spherical Harmonic spectrum of the density
+import matplotlib
+matplotlib.use('Tkagg')
 
 from ester import *
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 
-filename='M5_O95_nth30'
+filename='/users/p0107/mombarg/runs/AM_transport/M12_O50_X071Z120_evol_viscv1e7_visc_h1e7_Dv2e5_step050_0001.h5'
 a=star2d(filename) # open a 2D solution
 
-print 'Compute the 2D spectrum of the density'
+print('Compute the 2D spectrum of the density')
 
 # a.P is the projection matrix of the Gauss-Lobatto grid
 # a.P_00 is the projection matrix of the Gauss-Legendre grid for a field
@@ -16,14 +18,14 @@ print 'Compute the 2D spectrum of the density'
 
 
 sp_leg=np.zeros((a.nr,a.nth))
-print 'check the shape',sp_leg.shape
+print('check the shape',sp_leg.shape)
 
 # Gauss Legendre spectrum
 # we eliminate the pole/equator points that are respectively at a.nth+1
 # and 0
 
 for i in range(a.nr):
-    sp_leg[i,:]=np.dot(a.P_00,a.rho[i,1:a.nth+1]) 
+    sp_leg[i,:]=np.dot(a.P_00,a.vr[i,1:a.nth+1]) 
 
 for k in range(a.nth):
     sp_leg[:,k]=abs(np.dot(a.P,sp_leg[:,k]))
@@ -58,6 +60,6 @@ plt.yticks(jfirst)
 
 fig = plt.gcf()
 fig.set_size_inches(12., 10.,forward=True)
-plt.savefig(filename+'_sp2D.png',dpi=100, bbox_inches=0)
+plt.savefig('/users/p0107/mombarg/runs/AM_transport/2Dspec-vr_M12_O50_X071Z120_evol_viscv1e7_visc_h1e7_Dv2e5_step050_0001.png',dpi=200, bbox_inches=0)
 
 plt.show()