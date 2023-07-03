c0c2b859466370509997fd5112b7eb3e9c10353b
ctmbl <mabileau.clement@gmail.com>
2023-07-03 11:38:52 +0200

    Check that metal-mix.cfg keys are metals in a specific set and print the remaining of the sum of fractions

--- src/physics/composition.cpp
+++ src/physics/composition.cpp
@@ -6,6 +6,7 @@
 #include "physics.h"
 #include "parser.h"
 
+bool _init_metals = true;
 
 double_map initial_composition(double X, double Z) {
 	double_map comp;
@@ -17,6 +18,7 @@ double_map initial_composition(double X, double Z) {
 	// TODO: should this one be hardcoded too?
 	comp["He3"] = 3.15247417638132e-04 * Y;
 	comp["He4"] = Y - comp["He3"];
+	// Note that the previous way of setting He3 He4 ensure that He3+He4 = Y
 
 	// TODO: change this hardcoded
 	char file[] = "metal-mix.cfg";
@@ -28,6 +30,7 @@ double_map initial_composition(double X, double Z) {
 	for(std::string metal: metals){
 		comp[metal] = .0;
 	}
+	double metals_fraction = .0;
 
 	if(!fp.open(file)){
 		printf("Can't open configuration file %s\n", file);
@@ -40,11 +43,26 @@ double_map initial_composition(double X, double Z) {
 				printf("Syntax error in configuration file %s, line %d\n", file, line);
 				exit(1);
 			}
+			if(metals.find(arg) == metals.end()){
+				printf("%s is unknown, possible metals composition are: ", val);
+				for(std::string metal: metals){
+					printf("%s ", metal);
+				}
+				puts("\n");
+				exit(1);
+			}
+			metals_fraction += atof(val);
 			comp[arg] = Z * atof(val);
 		}
 	}
 	fp.close();
 	comp["Ex"] = 1 - comp.sum();
+
+	if(_init_metals){
+		printf("A config file %s has been used to config metal composition\n", file);
+		printf("The sum of metals fractions (of Z sum of metal mass fraction) is %f, and should be as close to 1 as possible.\n", metals_fraction);
+		_init_metals = false;
+	}
 
 	return comp;
 }
