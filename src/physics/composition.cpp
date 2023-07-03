8276b8ceb9a9479dac6ec2f3eb9bcbcd6081c98b
ctmbl <mabileau.clement@gmail.com>
2023-07-03 10:54:53 +0200

    Remove old metal comp initialization (commented)

--- src/physics/composition.cpp
+++ src/physics/composition.cpp
@@ -9,17 +9,6 @@ double_map initial_composition(double X, double Z) {
 	double_map comp;
 	file_parser fp;
 	
-	// The mixture below has an unidentified origin but is close to the one
-	// of Grevesse & Sauval 1998.
-	// comp["H"]=X;
-	// comp["He3"]=3.15247417638132e-04*(1.-X-Z);
-	// comp["He4"]=(1.-X-Z)-comp["He3"];
-	// comp["C12"]=Z*1.71243418737847e-01;
-	// comp["C13"]=Z*2.06388003380057e-03;
-	// comp["N14"]=Z*5.29501630871695e-02;
-	// comp["N15"]=Z*2.08372414940812e-04;
-	// comp["O16"]=Z*4.82006487350336e-01;
-	// comp["O17"]=Z*1.95126448826986e-04;
 	double Y = 1. - (X + Z);
 
 	comp["H"] = X;
