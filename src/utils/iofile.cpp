16f0afc321fa818a25a4155acb3ed2789ca41c6a
ctmbl <mabileau.clement@gmail.com>
2023-07-21 12:17:24 +0200

    Improve isHDF5Name iofile.cpp function

--- src/utils/iofile.cpp
+++ src/utils/iofile.cpp
@@ -5,20 +5,16 @@
 #include <stdlib.h>
 
 bool isHDF5Name(const char *fileName) {
-    char *name = strdup(fileName);
-    char *ext = name;
-    char *ptr = name;
     if (fileName == NULL) {
-        free(name);
         return false;
     }
-    while ((ptr = strstr(ext, ".")) != NULL) {
-        ext = ptr + 1;
+
+    const char *ext;
+    if((ext = strstr(fileName, ".")) == NULL) {
+        return false;
     }
-    if (strcasecmp(ext, "hdf5") == 0 || strcasecmp(ext, "h5") == 0) {
-        free(name);
+    if (strcasecmp(ext, ".hdf5") == 0 || strcasecmp(ext, ".h5") == 0) {
         return true;
     }
-    free(name);
     return false;
 }
