6a298adb3d86e01764a6de3c4a6c7a2d25ae5ddf
ctmbl <mabileau.clement@gmail.com>
2024-01-23 11:41:31 +0100

    Change src/include/parser.h indentation from tabs to 4 whitespaces

--- src/include/parser.h
+++ src/include/parser.h
@@ -11,29 +11,29 @@
 #include <clocale>
 
 inline void force_locale() {
-	setlocale(LC_NUMERIC, "C");
+    setlocale(LC_NUMERIC, "C");
 }
 
 class cmdline_parser {
-	int argc, i;
-	char **argv;
+    int argc, i;
+    char **argv;
 public:
-	cmdline_parser() { force_locale(); }
-	void open(int argc_in, char *argv_in[]);
-	int get(char *&arg, char *&val);
-	void ack(char *arg, char *val);
-	void close();
+    cmdline_parser() { force_locale(); }
+    void open(int argc_in, char *argv_in[]);
+    int get(char *&arg, char *&val);
+    void ack(char *arg, char *val);
+    void close();
 };
 
 class file_parser {
-	FILE *fp;
-	int iline;
-	char line[1024];
+    FILE *fp;
+    int iline;
+    char line[1024];
 public:
-	file_parser() { force_locale(); }
-	int open(const char *file);
-	int get(char *&arg,char *&val);
-	void close();
+    file_parser() { force_locale(); }
+    int open(const char *file);
+    int get(char *&arg,char *&val);
+    void close();
 };
 
 #endif
