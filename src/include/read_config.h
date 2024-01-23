613b8bd97f3e62dca24cbda5116a4e1440eda2a8
ctmbl <mabileau.clement@gmail.com>
2024-01-23 11:18:15 +0100

    Reorganize read_config.h

--- src/include/read_config.h
+++ src/include/read_config.h
@@ -7,6 +7,7 @@ public:
 	int minit,maxit;
 	double tol,newton_dmax;
 	int verbose;
+	bool noplot;
 	char plot_device[64];
 	double plot_interval;
 	char input_file[256];
@@ -14,13 +15,15 @@ public:
 	char output_file[256];
 
     configuration();
-	~configuration(){};
+    ~configuration(){};
+
     void read_config(int argc, char *argv[]); // wrapper around the following 2 methods
-	void read_config_file();
-	void read_command_line(int argc, char *argv[]);
+    void read_config_file();
+    void read_command_line(int argc, char *argv[]);
+
+    int check_arg(const char *arg,const char *val);
+
 	void missing_argument(const char *arg);
 	void unknown_parameter(const char *arg);
-	int check_arg(const char *arg,const char *val);
-    bool noplot;
 };
 
